624539a56f5a87929cb29a5a88d1db91459c5171
mukul <mukulsingh97087@gmail.com>
2024-09-13 22:02:38 +0530

    home

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -16,16 +16,16 @@
 <link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/default.css' }}" />
 <link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/style.css' }} " />
 <link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/responsive.css' }} " />
-<link rel="stylesheet" href="{{ url()->current() }}assets/front/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/front/css/animate.min.css">
-<link rel="stylesheet" href="assets/front/css/magnific-popup.css">
-<link rel="stylesheet" href="assets/front/fontawesome/css/all.min.css">
-<link rel="stylesheet" href="assets/front/css/dripicons.css">
-<link rel="stylesheet" href="assets/front/css/slick.css">
-<link rel="stylesheet" href="assets/front/css/meanmenu.css">
-<link rel="stylesheet" href="assets/front/css/default.css">
-<link rel="stylesheet" href="assets/front/css/style.css">
-<link rel="stylesheet" href="assets/front/css/responsive.css">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/bootstrap.min.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/animate.min.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/magnific-popup.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'fontawesome/css/all.min.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/dripicons.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/slick.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/meanmenu.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/default.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/style.css' }}">
+<link rel="stylesheet" href="{{ config('constants.options.Website_Files').'css/responsive.css' }}">
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
